7e6e531b3d0121d192ba639661788c472d1f4b38
Power <joost.rehder@uni-rostock.de>
2021-05-12 14:43:35 +0200

    classroomGui

--- src/main/java/GradePane.java
+++ src/main/java/GradePane.java
@@ -3,35 +3,50 @@ import javax.swing.*;
 public class GradePane {
     public JPanel panel1;
     private JPanel subpanel;
-    private JButton beitretenButton;
-    private JButton bearbeitenButton;
-    private JLabel terminLabel;
-    private JLabel lehrerLabel;
+    private JButton enterButton;
+    private JButton editButton;
+    private JLabel dateLabel;
+    private JLabel teacherLabel;
     private JLabel nextdatelabel;
     private JLabel thisteacherlabel;
     private JLabel gradeheader;
 
-    public GradePane(String grade, String thisteacher, String nextdate){
+    public GradePane(Language language, String grade, String thisteacher, String nextdate){
 
+        setupLanguage(language);
         this.gradeheader.setText(grade);
         this.thisteacherlabel.setText(thisteacher);
         this.nextdatelabel.setText(nextdate);
         this.subpanel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
     }
 
-
-    public GradePane(String beitreten, String bearbeiten, String lehrer, String termin, String grade, String thisteacher, String nextdate){
-
-        this.beitretenButton.setText(beitreten);
-        this.bearbeitenButton.setText(bearbeiten);
-        this.lehrerLabel.setText(lehrer);
-        this.terminLabel.setText(termin);
-        this.gradeheader.setText(grade);
-        this.thisteacherlabel.setText(thisteacher);
-        this.nextdatelabel.setText(nextdate);
-        this.subpanel.setBorder(BorderFactory.createRaisedSoftBevelBorder());
-
+    private void setupLanguage(Language language) {
+        String enter;
+        String edit;
+        String teacher;
+        String date;
+        switch (language) {
+            case german -> {
+                enter = "Beitreten";
+                edit = "Bearbeiten";
+                teacher = "Lehrer:";
+                date = "Termin:";
+            }
+            default -> {
+                enter = "Enter";
+                edit = "Edit";
+                teacher = "Teacher:";
+                date = "When?:";
+            }
+        }
+        setupGradePane(enter, edit, teacher, date);
     }
 
+    private void setupGradePane(String enter, String edit, String teacher, String date) {
 
+        this.enterButton.setText(enter);
+        this.editButton.setText(edit);
+        this.teacherLabel.setText(teacher);
+        this.dateLabel.setText(date);
+    }
 }
