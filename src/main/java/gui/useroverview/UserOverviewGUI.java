629f14a3b76541a1bd3c3e46eccc70e320535f3a
danielpassauer <passauer.daniel@gmail.com>
2021-05-17 21:30:23 +0200

    Added Foundation for UserOverviewGUI

---
+++ src/main/java/gui/useroverview/UserOverviewGUI.java
@@ -0,0 +1,30 @@
+package gui.useroverview;
+
+import gui.GUI;
+
+import javax.swing.*;
+
+public class UserOverviewGUI extends GUI {
+    private JPanel mainPanel;
+
+    public UserOverviewGUI() {
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        this.add(mainPanel);
+    }
+
+    private void setupGUI() {
+
+    }
+
+    public void updateGUI() {
+
+    }
+
+    private void setupListeners() {
+
+    }
+
+    private void initForAccountType() {
+
+    }
+}
