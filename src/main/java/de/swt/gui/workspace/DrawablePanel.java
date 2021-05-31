e8165a3ea2fd1155d45538c0893c025f7d2e9da8
danielpassauer <passauer.daniel@gmail.com>
2021-05-31 09:28:58 +0200

    Last

--- src/main/java/de/swt/gui/workspace/DrawablePanel.java
+++ src/main/java/de/swt/gui/workspace/DrawablePanel.java
@@ -205,17 +205,15 @@ public class DrawablePanel extends GUI {
             if (object == null) {
                 return;
             }
-            System.out.println(Arrays.toString(object.getID()) + " " + object);
             for (JComponent stackComponent : drawableObjects) {
                 DrawableObject stackObject = (DrawableObject) stackComponent;
-                System.out.println(Arrays.toString(stackObject.getID()) + " " + stackObject);
                 if (Arrays.equals(object.getID(), stackObject.getID())) {
-                    System.out.println("Removing " + stackObject + " because true");
                     componentsToRemove.add(stackObject);
                 }
             }
         }
         for (DrawableObject object1 : componentsToRemove) {
+            object1.closeAllPopups();
             drawPanel.remove(object1);
             drawableObjects.remove(object1);
         }
