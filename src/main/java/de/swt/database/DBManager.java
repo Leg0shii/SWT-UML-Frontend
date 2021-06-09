209fb0a1ee104a29895383d688414705f7655685
danielpassauer <passauer.daniel@gmail.com>
2021-06-09 11:35:38 +0200

    Made working with server

--- src/main/java/de/swt/database/DBManager.java
+++ src/main/java/de/swt/database/DBManager.java
@@ -7,7 +7,7 @@ public class DBManager {
     public AsyncMySQL connectToDB() {
         AsyncMySQL mySQL;
         try {
-            mySQL = new AsyncMySQL("5.196.174.213", 3306, "root", "qexGGHZfFzWyKYE", "swt-31");
+            mySQL = new AsyncMySQL("5.196.174.213", 3306, "root", "qexGGHZfFzWyKYE", "swt-db");
         } catch (SQLException | ClassNotFoundException exception) {
             exception.printStackTrace();
             return null;
